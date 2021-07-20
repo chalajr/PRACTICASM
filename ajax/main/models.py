d314975614d9d0ce484fcbda7bb059917c43e1c0
chalajr <chalajr2@gmail.com>
2021-07-20 11:20:26 -0500

    Base files created

--- ajax/main/models.py
+++ ajax/main/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+class example(models.Model):
+    title = models.CharField(max_length=50)
+    body = models.CharField(max_length=50)
+    date = models.CharField(max_length=50)
